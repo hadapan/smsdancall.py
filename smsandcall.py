7e1519e62c2bd19c8f695556e79a4213132af2c7
hadapan <69392219+hadapan@users.noreply.github.com>
2020-08-09 21:41:12 +0700

    Create smsandcall.py

---
+++ smsandcall.py
@@ -0,0 +1,70 @@
+#Coded By Zidan-ID\King Mr_Z17
+
+#YouTube Mr_Z17
+
+#Vortex Indonesia
+
+import os,sys,time
+
+ # SILAHKAN COSTUMISASI SENDIRI YA BOSQ #
+
+def main():
+
+    time.sleep(3)
+
+    os.system ('clear')
+
+    print '[96m'
+
+    os.system ('figlet Spam Work')
+
+    print'[92m================================='
+
+    print'[97m Author : [96mMasa Hadapan '
+
+    print'[97m Youtube  : [96mMasa Hadapan '
+
+    print'[92m================================='
+
+    print'[92m++++++++++++ [97mM E N U [92m++++++++++++'
+
+    print'[92m[[97m1[92m] [97mSpam Work '
+
+    print'[92m[[91m0[92m] [91mExit '
+
+    gans = raw_input ('[97m==>[93m ')
+
+    if gans in ['1']:
+
+        time.sleep(3)
+
+        os.system ('git clone https://github.com/Zidan-ID17/Spam-Tools')
+
+        print ('{+}Lalu Ketik ls')
+
+        print ('{+}Habis itu ketik cd Spam-Tools')
+
+        print ('{+}ketik ls lagi')
+
+        print ('{+}ketik python2 umberella.py')
+
+        exit()
+
+    if gans in ['0']:
+
+        time.sleep(3)
+
+        exit()
+
+    else:
+
+        time.sleep(1)
+
+        print '[97m Pilih Yang Bener [92mBro . . .'
+
+        time.sleep(3)
+
+        main()
+
+main()
+
